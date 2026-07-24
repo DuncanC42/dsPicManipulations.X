14060677a64dbb12f30be75a91c61104646c2f73
shi2gami42 <cadoret.du@gmail.com>
2026-07-24 10:35:16 +0300

    simple hello world on the oled screen

--- mcc_generated_files/i2c_host/i2c_host_types.h
+++ mcc_generated_files/i2c_host/i2c_host_types.h
@@ -1,16 +1,13 @@
 /**
- * PINS Generated Driver Header File 
+ * I2C Generated Driver Types Header File
  * 
- * @file      pins.h
- *            
- * @defgroup  pinsdriver Pins Driver
- *            
- * @brief     The Pin Driver directs the operation and function of 
- *            the selected device pins using dsPIC MCUs.
+ * @file      i2c_host_types.h
+ * 
+ * @ingroup   i2chostdriver
+ * 
+ * @brief     This is the generated driver types header file for the I2C driver using CCL
  *
- * @skipline @version   Firmware Driver Version 1.0.2
- *
- * @skipline @version   PLIB Version 1.4.1
+ * @skipline @version   PLIB Version 2.2.6
  *
  * @skipline  Device : dsPIC33CK64MC105
 */
@@ -36,21 +33,34 @@
     THIS SOFTWARE.
 */
 
-#ifndef PINS_H
-#define PINS_H
-// Section: Includes
-#include <xc.h>
+#ifndef I2C_HOST_TYPES_H
+#define    I2C_HOST_TYPES_H
 
-// Section: Device Pin Macros
+#include <stdint.h>
 
 /**
- * @ingroup  pinsdriver
- * @brief    Initializes the PINS module
- * @param    none
- * @return   none  
- */
-void PINS_Initialize(void);
+ @ingroup  i2chostdriver
+ @enum     I2C_HOST_ERROR
+ @brief    This Enum can be used to know the Host Error Status. 
+           Refer I2Cx_ErrorGet e.g. \ref I2C1_ErrorGet.
+*/
+enum I2C_HOST_ERROR
+{
+    I2C_HOST_ERROR_NONE,             /**< No Error */
+    I2C_HOST_ERROR_NACK,             /**< Client returned NACK */
+    I2C_HOST_ERROR_BUS_COLLISION,    /**< Bus Collision Error */
+};
 
-
+/**
+ @ingroup  i2chostdriver
+ @struct   I2C_TRANSFER_SETUP
+ @brief    I2C Clock Speed (100KHZ to 1MHZ)
+*/
+struct I2C_TRANSFER_SETUP
+{
+  uint32_t clkSpeed;            ///< I2C Clock Speed
+};
 
 #endif
+
+
